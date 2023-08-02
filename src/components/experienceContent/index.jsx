808409422c35644f0dfd7afe70d24ea67a78d5d2
MARLENE JOSSUE MUGA AVILA <marlene.m@bineo.com>
2023-08-02 02:36:34 -0600

    version que traduce a español e ingles

--- src/components/experienceContent/index.jsx
+++ src/components/experienceContent/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React ,{useContext}from 'react';
 import { useLocation } from 'react-router-dom';
-import experiences from '../../assets/data/experiences/index.json';
+//import experiences from '../../assets/data/experiences/index.json';
 import './styles.css';
+import { langContext } from '../../context/langContext';
+import { FormattedMessage } from 'react-intl';
 
 const ExperienceContent = () => {
+  const idioma2=useContext(langContext);
   const location = useLocation();
   const stateId = location?.pathname?.split('/')[2];
-  const getStateInfo = experiences?.experiences?.filter(experience => experience.id === stateId);
+  const getStateInfo = idioma2.messages.experiences?.filter(experience => experience.id === stateId);
   const data = getStateInfo?.[0];
 
   const getDescription = () => (
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Link } from 'react-router-dom';
 import Rss from '../Rss';
 import Logo from '../../assets/img/header/logo.png';
 import './styles.css';
+import { langContext } from '../../context/langContext';
+import { FormattedMessage } from 'react-intl';
 
 const Header = () => {
-  const listSecondary = [
+
+const idioma=useContext(langContext);
+
+  /*const listSecondary = [
     { name:"Calendario", 
       dir: "/calendario"
     },
@@ -16,7 +21,7 @@ const Header = () => {
     { name:"Descargas", 
       dir: "/descargas"
     }
-  ];
+  ];*/
 
   const listPrimary = [
     { name:"Tren Maya", 
@@ -36,32 +41,32 @@ const Header = () => {
       menu: [
         {
           name:"Bodas", 
-          dir: "/bodas",
+          dir: "/experiencias/bodas",
           image: "Pareja-Queretaro-3.webp"
         },
         {
           name:"Gastronomía", 
-          dir: "/gastronomia",
+          dir: "/experiencias/gastronomia",
           image: "Dgo_Ciudad_Durango421.webp"
         },
         {
           name:"Sol y playa", 
-          dir: "/sol-y-playa",
+          dir: "/experiencias/sol-y-playa",
           image: "Qroo_cancun_playa108.webp"
         },
         {
           name:"Aventura", 
-          dir: "/aventura",
+          dir: "/experiencias/aventura",
           image: "SIN_guanacoa_257.webp"
         },
         {
           name:"Bienestar", 
-          dir: "/bienestar",
+          dir: "/experiencias/bienestar",
           image: "MOR_Tepoztlan_HotelDosPiedras_LGBT-33.webp"
         },
         {
           name:"Sostenible", 
-          dir: "/sostenible",
+          dir: "/experiencias/sostenible",
           image: "BCS_tiburonballena.webp"
         }
       ]
@@ -74,24 +79,34 @@ const Header = () => {
     }
   ];
 
-  const listLanguages = [
+  /*const listLanguages = [
     { name:"Español", 
       value: "ES"
     },
     { name:"English", 
       value: "EN"
     }
-  ];
+  ];*/
 
-  const listMenuSecondary = () => (
+  const changeLenguage= event =>{
+    idioma.setLenguage(event.target.value);
+  }
+
+  /*const listMenuSecondary = () => (
     listSecondary?.map(item => (
-      <li className="header-menu-li-secondary" key={item.name}>
-        <Link to={item.dir}>
-          {item.name}
+      <li className="header-menu-li-secondary">
+        <Link to="/calendario">
+          <FormattedMessage id="calendar"  defaultMessage="Calendario"/>
+        </Link>
+        <Link to="/videos">
+          <FormattedMessage id="videos"  defaultMessage="Videos"/>
+        </Link>
+        <Link to="/descargas">
+          <FormattedMessage id="downloads"  defaultMessage="downloads"/>
         </Link>
       </li>
     ))
-  );
+  );*/
 
   const listMenuPrimary = () => (
     listPrimary?.map(item => (
@@ -119,13 +134,14 @@ const Header = () => {
     ))
   );
 
-  const languages = () => (
+  /*const languages = () => (
     listLanguages?.map(item => (
       <option value={item.value} key={item.name}>{item.name}</option>
     ))
-  );
+  );*/
 
   return (
+    
     <div className="header-ctn">
       
       <div className="header-ctn-logo">
@@ -138,19 +154,127 @@ const Header = () => {
       <nav className="header-ctn-menu">
         <div className="header-ctn-menu-ul-secondary">
           <ul className="header-menu-ul-secondary">
-            {listMenuSecondary()}
-            <select className="header-menu-select">
-              {languages()}
+              <li className="header-menu-li-secondary">
+                <Link to="/calendario">
+                  <FormattedMessage id="header.calendar"  defaultMessage="Calendrio test"/>
+              </Link>
+             </li>
+             <li className="header-menu-li-secondary">
+                <Link to="/videos">
+                    <FormattedMessage id="header.videos"  defaultMessage="Videos test"/>
+                </Link>
+              </li>
+              <li className="header-menu-li-secondary">
+                <Link to="/descargas">
+                    <FormattedMessage id="header.downloads"  defaultMessage="downloads test"/>
+                </Link>
+            </li>
+            <select className="header-menu-select" onChange={changeLenguage}>
+              <option  key="sp" value="sp">Español</option>
+              <option  key="en" value="en">Ingles</option>
             </select>
           </ul>
         </div>
         <div className="header-ctn-menu-ul-primary">
           <ul className="header-menu-ul-primary">
-            {listMenuPrimary()}
+            <li className="header-menu-li-primary" >
+      <Link to="/tren-maya">
+          <FormattedMessage id="header.train.maya"  defaultMessage="Tren Maya"/>
+      </Link>
+  </li>
+  <li className="header-menu-li-primary" >
+      <Link to="/destinos/Puebla">
+          <FormattedMessage id="header.destiny"  defaultMessage="Destinos"/>
+      </Link>
+  </li>
+  <li className="header-menu-li-primary" >
+      <Link to="/pueblos-magicos">
+          <FormattedMessage id="header.magic.downtowns"  defaultMessage="Pueblos Magicos"/>
+      </Link>
+  </li>
+  <li className="header-menu-li-primary" >
+      <Link to="/eventos-internacionales">
+          <FormattedMessage id="header.international.events"  defaultMessage="Eventos Internacionales"/>
+      </Link>
+  </li>
+  <li className="header-menu-li-primary" >
+    <Link to="/experiencias">
+        <FormattedMessage id="header.experiences"  defaultMessage="Experiencias"/>
+    </Link>
+      <div className="header-ctn-submenu-primary">
+            <div className="header-ctn-submenu-link-primary">
+              <Link to="/experiencias/bodas" className="header-submenu-link-primary">
+                  <FormattedMessage id="header.brides"  defaultMessage="Bodas"/>
+                </Link>
+                  <LazyLoadImage
+                    src={require(`../../assets/img/header/Pareja-Queretaro-3.webp`)}
+                    alt='logo visitmexico'
+                    className="header-submenu-link-img-primary"
+                  />
+            </div>
+            <div className="header-ctn-submenu-link-primary">
+              <Link to="/experiencias/rutas-gastronomicas" className="header-submenu-link-primary">
+                <FormattedMessage id="header.food"  defaultMessage="Gastronomía"/>
+              </Link>
+                <LazyLoadImage
+                  src={require(`../../assets/img/header/Dgo_Ciudad_Durango421.webp`)}
+                  alt='logo visitmexico'
+                  className="header-submenu-link-img-primary"/>
+            </div>
+            <div className="header-ctn-submenu-link-primary">
+              <Link to="/experiencias/sol-playa" className="header-submenu-link-primary">
+                  <FormattedMessage id="header.sun.beach"  defaultMessage="Sol y playa"/>
+                </Link>
+                  <LazyLoadImage
+                    src={require(`../../assets/img/header/Qroo_cancun_playa108.webp`)}
+                    alt='logo visitmexico'
+                    className="header-submenu-link-img-primary"/>
+             </div>
+             <div className="header-ctn-submenu-link-primary">
+              <Link to="/experiencias/aventura" className="header-submenu-link-primary">
+                  <FormattedMessage id="header.adventure"  defaultMessage="Aventura"/>
+                </Link>
+                  <LazyLoadImage
+                    src={require(`../../assets/img/header/SIN_guanacoa_257.webp`)}
+                    alt='logo visitmexico'
+                    className="header-submenu-link-img-primary"/>
+              </div>
+              <div className="header-ctn-submenu-link-primary">
+                <Link to="/experiencias/bienestar" className="header-submenu-link-primary">
+                    <FormattedMessage id="header.welfare"  defaultMessage="Bienestar"/>
+                  </Link>
+                    <LazyLoadImage
+                      src={require(`../../assets/img/header/MOR_Tepoztlan_HotelDosPiedras_LGBT-33.webp`)}
+                      alt='logo visitmexico'
+                      className="header-submenu-link-img-primary"/>
+              </div>
+              <div className="header-ctn-submenu-link-primary">
+              <Link to="/experiencias/sostenible" className="header-submenu-link-primary">
+                  <FormattedMessage id="header.sustainable"  defaultMessage="Sostenible"/>
+                </Link>
+                  <LazyLoadImage
+                    src={require(`../../assets/img/header/BCS_tiburonballena.webp`)}
+                    alt='logo visitmexico'
+                    className="header-submenu-link-img-primary"/>
+          </div>
+      </div>
+      
+ </li>
+  <li className="header-menu-li-primary" >
+      <Link to="/blog">
+          <FormattedMessage id="header.blog"  defaultMessage="Blog"/>
+      </Link>
+  </li>
+  <li className="header-menu-li-primary" >
+      <Link to="/guias-turisticas">
+          <FormattedMessage id="header.tourist.guides"  defaultMessage="Guías Turísticas"/>
+      </Link>
+  </li>
           </ul>
         </div>
       </nav>
     </div>
+   
   );
 };
 
